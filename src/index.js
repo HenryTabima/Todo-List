bbe76b8e90aa306521b57b69fa5cd8acbf2615cf
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-07-03 17:02:26 -0500

    [add] sumbit listners for projects and task forms

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import Todo from './models/todo'
 import Collection from './lib/collection'
 import DOMHandler from './lib/domHandler'
 
-const projectList = new Collection(Project)
-const todoList = new Collection(Todo)
+document.addEventListener('DOMContentLoaded', () => {
+  const projectList = new Collection(Project)
+  const todoList = new Collection(Todo)
 
-DOMHandler.init({ todoList, projectList })
+  projectList.addItem({ title: 'Default', description: 'Default project' })
+
+  DOMHandler.init({ todoList, projectList })
+})
--- src/lib/domHandler.js
+++ src/lib/domHandler.js
@@ -2,6 +2,21 @@
 
 function init ({ todoList, projectList }) {
   console.log('Initializating DOM')
+
+  const $projectForm = document.getElementById('new-project-form')
+  $projectForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const projectTitle = event.target[0].value
+    const projectDescription = event.target[1].value
+    console.log({ title: projectTitle, description: projectDescription })
+    $projectForm.reset()
+  })
+  const $taskForm = document.getElementById('new-task-form')
+  $taskForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log('Create Task', event.target)
+    $taskForm.reset()
+  })
 }
 
 export default { init }

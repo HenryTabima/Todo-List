1c716c4b34ba2e455b0943cc9eb61332b27ec8f1
Sunny <ring2rings93@gmail.com>
2019-07-04 16:59:06 +0100

    projects create store

--- src/lib/domHandler.js
+++ src/lib/domHandler.js
@@ -1,22 +1,41 @@
 'use strict'
+let lists = {}
+let DomElements = {}
 
 function init ({ todoList, projectList }) {
   console.log('Initializating DOM')
+  lists = { todos: todoList, projects: projectList }
 
-  const $projectForm = document.getElementById('new-project-form')
-  $projectForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const projectTitle = event.target[0].value
-    const projectDescription = event.target[1].value
-    console.log({ title: projectTitle, description: projectDescription })
-    $projectForm.reset()
-  })
-  const $taskForm = document.getElementById('new-task-form')
-  $taskForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    console.log('Create Task', event.target)
-    $taskForm.reset()
-  })
+  DomElements.projectForm = document.getElementById('new-project-form')
+  DomElements.projectForm.addEventListener('submit', onProjectCreate)
+
+  // const $taskForm = document.getElementById('new-task-form')
+  // $taskForm.addEventListener('submit', (event) => {
+  //   event.preventDefault()
+  //   console.log('Create Task', event.target)
+  //   $taskForm.reset()
+  // })
 }
+
+function onProjectCreate (event) {
+  event.preventDefault()
+  const projectParams = {
+    title: event.target[0].value,
+    description: event.target[1].value
+  }
+  lists.projects.addItems(projectParams)
+  onProjectChange()
+  DomElements.projectForm.reset()
+}
+
+function onProjectChange () {
+  let collection = lists.projects.getCollection()
+  let row = document.createElement('tr')
+
+}
+
+// function addProjectDOM (title, description) {
+//   let row = document.createElement('tr')
+// }
 
 export default { init }
